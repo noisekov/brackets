24767b6cd6e176d2f12486d73fab42cfa0989996
Vladimir <noise123q@mail.ru>
2023-02-12 00:43:43 +0300

    feat: more test pass

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
+    const bracketsObj = bracketsConfig.reduce((acc, item) => {
+    acc[item[1]] = item[0];
+    return acc;
+  }, {})
+  
+  const openbrackets = [];
+  bracketsConfig.forEach(x => {
+    openbrackets.push(x[0])
+  })
+ 
   const stack = [];
   const strArr = str.split('');
-  for (let i = 0; i < strArr.length;i++) {
-    for (let j = 0; j < bracketsConfig.length;j++) {
-      if(bracketsConfig[j][0] === bracketsConfig[j][1] && str.length % 2 ===0) {
-        break;
+
+  for (let i = 0; i < strArr.length; i++) {
+        let currentStr = strArr[i];
+        let topElement = stack.at(-1);
+    
+    console.log()
+    if (openbrackets.includes(currentStr) && topElement !== currentStr) {
+      stack.push(currentStr)
+    } else {
+      if (stack.length === 0) {
+        return false
+      }
+      
+      if(bracketsObj[currentStr] === topElement) {
+        stack.pop()
       } else {
-        if(strArr[i] === bracketsConfig[j][0]) {
-          stack.push(strArr[i]);
-        } else if (strArr[i] === bracketsConfig[j][1]) {
-          if(!stack.pop()) {
-            break
-          } else {
-            stack.pop();
-          }
-        } 
+        return false
       }
     }
   }

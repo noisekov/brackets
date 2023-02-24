a07a9f6d6d3c8346687eca1ba2907798486f736a
Vladimir <noise123q@mail.ru>
2023-02-24 23:58:47 +0300

    feat: pass more test

--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-    const bracketsObj = bracketsConfig.reduce((acc, item) => {
+  const bracketsObj = bracketsConfig.reduce((acc, item) => {
     acc[item[1]] = item[0];
     return acc;
   }, {})
   
   const openbrackets = [];
+  const closebrackets = [];
+  const theSame = [];
+
   bracketsConfig.forEach(x => {
-    openbrackets.push(x[0])
+    openbrackets.push(x[0]);
+    closebrackets.push(x[1]);
+    if (x[1] === x[0]) {
+    theSame.push(x[0])
+  }
   })
- 
+
   const stack = [];
   const strArr = str.split('');
 
   for (let i = 0; i < strArr.length; i++) {
+
         let currentStr = strArr[i];
         let topElement = stack.at(-1);
     
-    console.log()
-    if (openbrackets.includes(currentStr) && topElement !== currentStr) {
+    if (currentStr === theSame[0]) {
+      if (stack.length === 0) {
+        stack.push(currentStr)   
+      } 
+      
+      if (topElement === theSame[0]) {
+        stack.pop()
+      }
+    } else {
+          if (openbrackets.includes(currentStr)) {
       stack.push(currentStr)
     } else {
+
       if (stack.length === 0) {
         return false
       }
@@ -29,7 +46,11 @@ module.exports = function check(str, bracketsConfig) {
       } else {
         return false
       }
+
     }
+    }
+   
   }
+  console.log(stack)
   return stack.length === 0;
 }
